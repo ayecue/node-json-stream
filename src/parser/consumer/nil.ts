88ddb647ad3672e5736977eb6e5ad86f33572fb8
ayecue <soerenwehmeier@googlemail.com>
2023-07-02 20:05:40 +0200

    null uses 3 bytes

--- src/parser/consumer/nil.ts
+++ src/parser/consumer/nil.ts
@@ -3,7 +3,7 @@ import { BaseConsumer, ConsumerState, ConsumerType } from './base';
 
 export class NilConsumer extends BaseConsumer {
   protected _data: null = null;
-  protected _size: number = 0;
+  protected _size: number = 3;
   protected _type: ConsumerType = ConsumerType.Nil;
 
   consume(_item: TokenResult): boolean {
